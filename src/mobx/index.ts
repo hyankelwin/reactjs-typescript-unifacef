fc20099fa4b275944a920698d6a58fec6cdd396d
hyan-kelwin <hyankelwin@hotmail.com>
2022-04-11 09:26:47 -0300

    feat: config mobx

---
+++ src/mobx/index.ts
@@ -0,0 +1,19 @@
+import { home } from '../containers/home/store';
+import { combustivel } from '../containers/combustivel/store';
+import { router } from './router.store';
+import { starWars } from '../containers/star-wars/store';
+import { starWarsDetails } from '../containers/star-wars-details/store';
+import { register } from '../containers/register/store'
+import { cache } from '../containers/cache/store';
+import { eleicoes } from '../containers/eleicoes/store';
+
+export {
+  router,
+  combustivel,
+  home,
+  starWars,
+  starWarsDetails,
+  register,
+  cache,
+  eleicoes
+}
